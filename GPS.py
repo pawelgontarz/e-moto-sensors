5684ab716d94c1c0281f0f861601858a729b1ec3
Paweł Gontarz <gontarzpawe@gmail.com>
2019-06-01 11:17:28 +0200

    Add auto-finding GPS device

--- GPS.py
+++ GPS.py
@@ -1,21 +1,27 @@
 from threading import Thread
 import time
 import serial
-
+import serial.tools.list_ports
+import re
 
 class GPSHandler(Thread):
     def __init__(self, parameters):
         super().__init__()
         self.lat = 0
         self.lon = 0
+        self.USB_port = ""
         self.running = True
+        self.debug = False
         self.parameters = parameters
 
     def run(self):
+        #detecting USB device
+        self.findDevice()
+        # connect with device
         while True:
-            print("[GPS] Start connecting to GPS port...")
+            print("[GPS THREAD] Start connecting to GPS port...")
             try:
-                self.gps = serial.Serial("/dev/ttyACM0", baudrate= 115200)
+                self.gps = serial.Serial(self.USB_port, baudrate= 115200)
                 break
             except ValueError:
                 print("Something goes wrong...")
@@ -23,14 +29,14 @@ class GPSHandler(Thread):
             except:
                 print("No port detected...")
                 time.sleep(1)
-        print("[GPS] Connected to GPS port succesfully!")
-
+        print("[GPS THREAD] Connected to GPS port successfully.")
+        #main loop
         while self.running:
             dataLine = self.gps.readline()
-            self.getCoordinates(dataLine)
-            self.getSpeed(dataLine)
+            self.getCoordinates(dataLine,self.debug)
+            self.getSpeed(dataLine,self.debug)
     
-    def getCoordinates(self,dataLine):
+    def getCoordinates(self,dataLine, debug):
         data = dataLine.decode().split(",")
         if data[0] == '$GPRMC':
             try:
@@ -48,20 +54,35 @@ class GPSHandler(Thread):
                     longitude = 1*(longitudeDegree + longitudeMin)
                 elif longitudeMark == "W":
                     longitude = -1*(longitudeDegree + longitudeMin)
-                #print(str(latitude) + ' ' + str(longitude))
                 self.parameters.lat = latitude
                 self.parameters.lon = longitude
+                if debug == True:
+                    print(str(latitude) + ' ' + str(longitude))
             except:
                 print("[GPS THREAD] Lat/lon error!")
 
-    def getSpeed(self,dataLine):
+    def getSpeed(self,dataLine, debug):
         data = dataLine.decode().split(",")
         if data[0] == "$GPVTG": 
             try:
                 speed = round(float(data[7]))
-                #print("Speed: " + str(speed))
                 #moto_parameters.speed = speed + random.randint(1,40)
                 self.parameters.speed = speed
+                if debug == True:
+                    print("Speed: " + str(speed))
             except:
                 print("[GPS THREAD] Speed error!")
 
+    def findDevice(self):
+        ports = list(serial.tools.list_ports.grep('/dev/ttyACM*'))
+        regexp = re.compile(r'GPS')
+        for port in ports:
+            while True:
+                print("[GPS THREAD] Scanning USB Ports")
+                if regexp.search(str(port)):
+                    print('[GPS THREAD] GPS module: ' + str(port.product) + " detected on port: " + str(port.device))
+                    self.USB_port = port.device
+                    break
+                else:
+                    print("[GPS THREAD] Can't find GPS USB device")
+                time.sleep(0.5)
